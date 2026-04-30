53498ef02e61e2d1f9925186ec30e630f8151a80
javier guerra <javierguerradevelop@gmail.com>
2026-04-29 21:03:43 -0400

    Implementing parser and testing functionality

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ mod socket_sender;
 mod writer;
 
 fn main() {
+    /*
     let (tx, rx) = mpsc::channel::<String>();
     let filepath = "testpath.txt";
 
@@ -26,4 +27,9 @@ fn main() {
     for line in rx {
         parser::parse_line(&line);
     }
+    */
+    let test_line = String::from(
+        r#"6/14/2025 18:03:27.701-4  SPELL_DAMAGE,Player-1427-0E1E8D11,"Orçaos-Ragnaros-US",0x512,0x20,Creature-0-4218-2661-9671-214668-0003CDF1A1,"Venture Co. Patron",0xa48,0x0,57755,"Heroic Throw",0x1,Creature-0-4218-2661-9671-214668-0003CDF1A1,0000000000000000,117076402,117152954,0,0,42857,0,0,0,1,0,0,0,2643.93,-4917.11,2335,2.4201,80,76552,109359,-1,1,0,0,0,nil,nil,nil,ST"#,
+    );
+    parser::parse_line(&test_line);
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,15 +1,69 @@
-struct CombatEvent {
+pub struct CombatEvent {
     timestamp: String,
     event_type: String,
-    name: String,
+    source_name: String,
     source_id: String,
     target_id: String,
     source_raid_flag: String,
     spell_name: String,
-    npc_id: String,
     spell_id: i32,
 }
 
-pub fn parse_line(line: &str) {
-    let parsed: Vec<&str> = line.split(',').collect();
+impl CombatEvent {
+    pub fn new(
+        timestamp: String,
+        event_type: String,
+        source_name: String,
+        source_id: String,
+        target_id: String,
+        source_raid_flag: String,
+        spell_name: String,
+        spell_id: i32,
+    ) -> Self {
+        Self {
+            timestamp,
+            event_type,
+            source_name,
+            source_id,
+            target_id,
+            source_raid_flag,
+            spell_name,
+            spell_id,
+        }
+    }
+}
+
+pub fn parse_line(line: &str) -> Option<CombatEvent> {
+    let mut parts = line.split(',');
+
+    let prefix = parts.next()?;
+    let source_guid = parts.next()?;
+    let source_name = parts.next()?;
+    let _source_flags = parts.next()?;
+    let source_raid_flags = parts.next()?;
+    let dest_guid = parts.next()?;
+    let _dest_name = parts.next()?;
+    let _dest_flags = parts.next()?;
+    let _dest_raid_flags = parts.next()?;
+    let spell_id = parts.next()?;
+    let spell_name = parts.next()?;
+
+    println!("prefix: {}", prefix);
+    println!("source_name: {}", source_name);
+    println!("source_guid: {}", source_guid);
+    println!("dest_guid: {}", dest_guid);
+    println!("source_raid_flags: {}", source_raid_flags);
+    println!("spell_name: {}", spell_name);
+    println!("spell_id: {}", spell_id);
+
+    Some(CombatEvent::new(
+        String::from(prefix),
+        String::from(source_name),
+        String::from(source_guid),
+        String::from(dest_guid),
+        String::from(source_raid_flags),
+        String::from(spell_name),
+        String::from("1"),
+        spell_id.parse::<i32>().unwrap_or(0),
+    ))
 }
